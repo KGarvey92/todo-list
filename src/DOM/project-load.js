56b9d40a3c5489d43410a34f86971a0807e69aaf
KGarvey92 <kgarvey92@gmail.com>
2023-02-16 12:21:53 +0000

    fix multiple event listeners bug on submitBtn

--- src/DOM/project-load.js
+++ src/DOM/project-load.js
@@ -2,25 +2,21 @@ import { saveUpdates } from '../app/Project';
 import Todo from '../app/Todo';
 
 function addSubmitListener(e, project) {
-  // prevent default behaviour of submit button
   e.preventDefault();
-  // Capture input
   const title = document.querySelector('#title-input');
   const description = document.querySelector('#description-input');
   const dueDate = document.querySelector('#due-date-input');
   const priority = document.querySelector('#priority-select');
 
-  // Create new todo using input
   const todo = new Todo(title.value, description.value, priority.value, dueDate.value);
-  // add todo to current project
   project.addTodo(todo);
   saveUpdates();
-  // Reset form
+
   title.value = '';
   description.value = '';
   dueDate.value = '';
   priority.value = 'normal';
-  // close form and remove overlay after submit
+
   const overlay = document.querySelector('#overlay');
   const form = document.querySelector('#todo-form');
   overlay.style.display = 'none';
@@ -29,8 +25,49 @@ function addSubmitListener(e, project) {
 
 function prepSubmitBtn(project) {
   const submitButton = document.querySelector('#submit-btn');
-  submitButton.removeEventListener('click', addSubmitListener);
-  submitButton.addEventListener('click', (e) => addSubmitListener(e, project));
+
+  submitButton.addEventListener('click', (e) => {
+    addSubmitListener(e, project);
+  }, { once: true });
+}
+
+function displayTodos(project) {
+  const main = document.querySelector('main');
+
+  // Loop through todos array and display each in a div
+  for (let i = 0; i < project.todos.length; i += 1) {
+    // Create div to hold todo
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo-div');
+
+    // Create checkmark and append
+    const check = document.createElement('img');
+    check.setAttribute('src', 'images/icons/check.svg');
+    check.setAttribute('alt', 'checkmark icon');
+    todoDiv.appendChild(check);
+
+    // TODO: attach event listener to change todo.completed to
+    // true and also add a class to change color or strikethrough.
+
+    // Create <p> to hold todo title
+    const todo = document.createElement('p');
+    todo.innerText = `${project.todos[i].title}`;
+    todoDiv.appendChild(todo);
+
+    // TODO: Add event listener to todo title to open up
+    // a todo display popup that shows duedate, description and allows editing.
+
+    // Create remove icon
+    const removeBtn = document.createElement('img');
+    removeBtn.setAttribute('src', 'images/icons/remove.svg');
+    removeBtn.setAttribute('alt', 'remove icon');
+    todoDiv.appendChild(removeBtn);
+
+    // TODO: event listener to call removeTodo method on click.
+
+    // Append todo to main.
+    main.appendChild(todoDiv);
+  }
 }
 
 function loadProject(project) {
@@ -46,15 +83,14 @@ function loadProject(project) {
 
   // Display project description
   const description = document.createElement('p');
+  description.classList.add('project-desciription');
   description.innerText = `${project.description}`;
   main.appendChild(description);
 
-  // Display first todo (later adapt to use a loop and display all todos)
-  const todo1 = document.createElement('div');
-  const title = document.createElement('p');
-  title.innerText = `${project.todos[0].title}`;
-  todo1.appendChild(title);
-  main.appendChild(todo1);
+  // Display todos
+  if (project.todos.length !== 0) {
+    displayTodos(project);
+  }
 
   // Create an add todo button
   const addTodoBtn = document.createElement('button');
