9518196a6567d2ac8828ae558118fd249c9fb372
KGarvey92 <kgarvey92@gmail.com>
2023-03-01 13:40:34 +0000

    project container styling

--- src/DOM/projects-page.js
+++ src/DOM/projects-page.js
@@ -1,3 +1,32 @@
+import { projects } from '../app/Project';
+
+function createProjectList() {
+  // loop through projects and create div for each
+  const main = document.querySelector('main');
+  projects.forEach((project) => {
+    const container = document.createElement('div');
+    container.classList.add('project-container');
+    const title = document.createElement('p');
+    title.innerText = project.title;
+    title.classList.add('project-title');
+    container.appendChild(title);
+    const description = document.createElement('p');
+    description.classList.add('project-description');
+    if (project.description) {
+      description.innerText = project.description;
+    } else {
+      description.innerText = 'Add a description';
+    }
+    container.appendChild(description);
+    const removeBtn = document.createElement('img');
+    removeBtn.setAttribute('src', 'images/icons/remove.svg');
+    removeBtn.setAttribute('alt', 'remove project icon');
+    removeBtn.classList.add('remove-button');
+    container.appendChild(removeBtn);
+    main.appendChild(container);
+  });
+}
+
 function displayProjects() {
   const main = document.querySelector('main');
   main.innerText = '';
@@ -5,10 +34,10 @@ function displayProjects() {
   heading.classList.add('content-headings');
   heading.innerText = 'Projects';
   main.appendChild(heading);
-  const sample = document.createElement('div');
-  sample.classList.add('projects');
-  sample.innerText = 'Feature coming soon';
-  main.appendChild(sample);
+  const note = document.createElement('p');
+  note.innerText = 'Click on title or description to edit.';
+  main.appendChild(note);
+  createProjectList();
 
   return main;
 }
